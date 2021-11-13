c49cccfaae0e45e4e870c547185565849096e4fc
JosueFS <josue.18rj@gmail.com>
2021-11-13 09:54:10 -0300

    fix: hidden API_KEY

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-// const API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = process.env.VUE_APP_TMDB_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3/';
+
+console.log(API_KEY);
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -13,8 +15,6 @@ const api = axios.create({
 
 export default {
   getMovieImages(movieId) {
-    return api.get(
-      `/movie/${movieId}/images?api_key=61a2a020dd3de41e469bc09fc266744f`
-    );
+    return api.get(`/movie/${movieId}/images?api_key=${API_KEY}`);
   },
 };
