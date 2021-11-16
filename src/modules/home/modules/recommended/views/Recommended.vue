de91a876a17dcfb8651502e60f810e79dad010a0
JosueFS <josue.18rj@gmail.com>
2021-11-16 08:55:18 -0300

    feat: Tips for new users about recommending movies

--- src/modules/home/modules/recommended/views/Recommended.vue
+++ src/modules/home/modules/recommended/views/Recommended.vue
@@ -6,11 +6,22 @@
       listTitleKey="title"
       listTitle="Recommended Movies"
     />
+
+    <v-col class="mt-4 text-center" v-if="!movies || !movies.length">
+      <v-icon size="96">mdi-emoticon-sad</v-icon>
+      <h4>
+        We still can't recommend any movies,
+        <a href="/home/discover">go to movie discovery</a> and rate them.
+      </h4>
+      <p class="caption">
+        Remembering that we will recommend movies based on ratings above 3
+        stars.
+      </p>
+    </v-col>
   </v-container>
 </template>
 
 <script>
-//TODO: ADD Tips to see Discover Page when not have movies to recommend
 import RecommendationService from './../services/recommended.service';
 
 import GroupMovieList from '@/modules/home/components/GroupMovieList.vue';
